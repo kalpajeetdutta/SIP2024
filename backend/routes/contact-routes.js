0cf7e6ec86cdd2a39a50989c89dbade8c5673536
kalpajeetdutta <kalpajeetdutta30@gmail.com>
2024-07-09 23:20:40 +0530

    backend folder removed from frontend

--- backend/routes/contact-routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {sentMsg} from "../controllers/contact-controller.js"
-
-const router = express.Router();
-
-router.post('/add',sentMsg)
-
-export default router;
